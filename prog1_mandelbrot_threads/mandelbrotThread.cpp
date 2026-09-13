a7e2470f855cf9841d98a83679ac880cb411faff
Zeeshan Zahid <zeeshanzahid142004@gmail.com>
2026-09-13 13:52:05 +0500

    Implement basic spatial decomposition for worker threads

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -29,16 +29,16 @@ extern void mandelbrotSerial(
 //
 // Thread entrypoint.
 void workerThreadStart(WorkerArgs * const args) {
+    int startRow = (args->threadId * args->height) / args->numThreads;
+    int endRow = ((args->threadId + 1) * args->height) / args->numThreads;
+    int numRows = endRow - startRow; // Calculate exactly how many rows this thread owns
 
-    // TODO FOR CS149 STUDENTS: Implement the body of the worker
-    // thread here. Each thread should make a call to mandelbrotSerial()
-    // to compute a part of the output image.  For example, in a
-    // program that uses two threads, thread 0 could compute the top
-    // half of the image and thread 1 could compute the bottom half.
-
-    printf("Hello world from thread %d\n", args->threadId);
+    mandelbrotSerial(args->x0, args->y0, args->x1, args->y1,
+                     args->width, args->height,
+                     startRow, numRows,
+                     args->maxIterations,
+                     args->output);
 }
-
 //
 // MandelbrotThread --
 //
